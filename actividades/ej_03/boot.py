3a822aac19df458facd151a175987646e236bb62
DaniloDiaz35 <82105689+DaniloDiaz35@users.noreply.github.com>
2024-11-16 20:23:37 -0300

    Update boot.py

--- actividades/ej_03/boot.py
+++ actividades/ej_03/boot.py
@@ -1,20 +1,12 @@
 def do_connect():
     import network
-    # Configuración de la interfaz Wi-Fi en modo estación
-    wifi_interface = network.WLAN(network.STA_IF)
-
-    # Verificación de conexión; si no está conectado, intenta conectarse
-    if not wifi_interface.isconnected():
-        print('Conectando a la red Wi-Fi...')
-        wifi_interface.active(True)
-        wifi_interface.connect('Cooperadora Alumnos', '')  # Nombre de la red Wi-Fi y contraseña
-
-        # Espera hasta establecer conexión
-        while not wifi_interface.isconnected():
-            pass
-
-    # Muestra la configuración de la red si la conexión fue exitosa
-    print('Configuración de red:', wifi_interface.ifconfig())
-
-# Llama a la función de conexión Wi-Fi al iniciar la placa
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('Conectando a la red...')
+        sta_if.active(True)
+        sta_if.connect('Cooperadora Alumnos', '')
+        while not sta_if.isconnected():
+            print(".", end = "")
+    print('network config:', sta_if.ifconfig())
+    
 do_connect()
